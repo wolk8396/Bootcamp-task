2c42abc1d8123e82cede6505bfa84480352d398e
wolk8396 <mastertso2241@gmail.com>
2023-03-23 20:35:14 +0300

    second

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AvatarComponent } from "./cart.character.component/cart.character.component";
+import { HeaderComponentApp } from "./header.component/header.component";
 
 @NgModule({
   declarations : [
-    AvatarComponent
+    AvatarComponent,
+    HeaderComponentApp
   ],
   imports:[
     CommonModule,
 
   ],
   exports: [
-    AvatarComponent
+    AvatarComponent,
+    HeaderComponentApp
   ],
 })
 export class ComponentModule {}
---
+++ src/app/components/header.component/header.component.ts
@@ -0,0 +1,37 @@
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from "@angular/core";
+
+@Component({
+  selector: 'app-header-component',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponentApp implements OnInit {
+  isActive: boolean = false;
+  isPosition: boolean = false;
+
+  @ViewChild('nav') nav!: ElementRef;
+
+  constructor(
+    public renderer: Renderer2,
+  ) {}
+
+  ngOnInit(): void {
+
+  }
+
+  onBurgerMenu(): void {
+    this.isActive = !this.isActive;
+
+    (this.isActive) ?
+      this.renderer.addClass(document.body, 'isActive'):
+      this.renderer.removeClass(document.body, 'isActive');
+  }
+
+  onPositionPage(position: ScrollLogicalPosition): void {
+    document.body.scrollIntoView({
+      behavior: "smooth",
+      block: position,
+      inline: "nearest"
+    });
+  }
+}
